4f41a673bb757ef71e04b3b72cd5bc0a25287120
junfus <junfus1024@gmail.com>
2019-06-03 13:55:17 -0400

    add fish aliases

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+# personal aliases of git
+alias gcl='git clone'
+alias ga='git add'
+alias grm='git rm'
+alias gst='git status'
+alias gcm='git commit -m'
+alias gt="git tag"
+alias gpl='git pull'
+alias gps='git push'
